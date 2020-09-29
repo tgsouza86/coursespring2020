7b8a918f6b9e33d3338aecd17b6a1c575bc59ea8
Gonçalves de Souza <togz@gft.com>
2020-09-29 07:41:04 -0300

    Inclusion of H2 database

--- src/main/java/com/tgsouza/coursespring/entities/User.java
+++ src/main/java/com/tgsouza/coursespring/entities/User.java
@@ -2,9 +2,19 @@ package com.tgsouza.coursespring.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class User implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
@@ -14,7 +24,8 @@ public class User implements Serializable{
 	public User() {
 		
 	}
-
+	
+	
 	public User(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
