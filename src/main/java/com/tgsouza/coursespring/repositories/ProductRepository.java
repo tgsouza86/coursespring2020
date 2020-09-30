e361364bf03b8a40ed1f77c3867241a6399ca788
Gonçalves de Souza <togz@gft.com>
2020-09-30 19:44:11 -0300

    Entities Product

---
+++ src/main/java/com/tgsouza/coursespring/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.tgsouza.coursespring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tgsouza.coursespring.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
---
+++ src/main/java/com/tgsouza/coursespring/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.tgsouza.coursespring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tgsouza.coursespring.entities.Product;
+import com.tgsouza.coursespring.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id)
+	{
+		Optional<Product>  obj = repository.findById(id);
+		return obj.get();
+	}
+}
