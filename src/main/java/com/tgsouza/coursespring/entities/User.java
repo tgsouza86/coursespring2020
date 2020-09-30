180e3eba30990e596a1e5f9efdf4733b83d2c4bf
Gonçalves de Souza <togz@gft.com>
2020-09-30 14:27:42 -0300

    Entidade Pedido - Datas com Instante - parte 1

--- src/main/java/com/tgsouza/coursespring/entities/User.java
+++ src/main/java/com/tgsouza/coursespring/entities/User.java
@@ -1,14 +1,19 @@
 package com.tgsouza.coursespring.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 
 	private static final long serialVersionUID = 1L;
@@ -21,6 +26,8 @@ public class User implements Serializable{
 	private String phone;
 	private String password;
 	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders =new ArrayList<Order>();
 	public User() {
 		
 	}
