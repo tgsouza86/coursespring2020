5607347b4276326c509326d692eddf09e7aaa875
Gonçalves de Souza <togz@gft.com>
2020-11-01 14:32:06 -0300

    Tratamento de Exceção findby

--- src/main/java/com/tgsouza/coursespring/services/UserService.java
+++ src/main/java/com/tgsouza/coursespring/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.tgsouza.coursespring.entities.User;
 import com.tgsouza.coursespring.repositories.UserRepository;
+import com.tgsouza.coursespring.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +23,7 @@ public class UserService {
 	public User findById(Long id)
 	{
 		Optional<User>  obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
---
+++ src/main/java/com/tgsouza/coursespring/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,15 @@
+package com.tgsouza.coursespring.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id" + id);
+	}
+
+
+}
